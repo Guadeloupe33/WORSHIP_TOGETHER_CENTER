77a14399a0a3d8b0c0908dd0c20990b15f7ea677
Steven Francius <stevenfrancius@gmail.com>
2025-08-05 16:26:43 -0400

    added addtinal funactionality to the server

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,11 @@ connectDB();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:5501', 'http://127.0.0.1:5502'],
-    credentials: true
-  }));
+  origin: ['http://localhost:5501', 'http://127.0.0.1:5501', 'http://localhost:5502', 'http://127.0.0.1:5502'],
+  credentials: true
+}));
+
+
   
 
 // Routes
