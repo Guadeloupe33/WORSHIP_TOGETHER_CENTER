ac48c1c8a566ada9e9feb0a1e572216d6c3bb015
Steven Francius <stevenfrancius@gmail.com>
2025-09-10 13:00:21 -0400

    updated cor

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:5501', 'http://127.0.0.1:5501', 'http://localhost:5502', 'http://127.0.0.1:5502'],
+  origin: ['http://localhost:5501', 'http://127.0.0.1:5501', 'http://localhost:5502', 'http://127.0.0.1:5502','http://localhost:5500'],
   credentials: true
 }));
 
